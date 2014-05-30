ddc39a07d230eb37d2f1de88f6974092f115646b
Yoren Chang <yorenchang@gmail.com>
2014-05-30 19:10:25 +0800

    Initial commit

---
+++ README.txt
@@ -0,0 +1,50 @@
+=== Custom Featured Image Metabox ===
+
+Contributors: 1fixdotio, yoren
+Donate link: http://1fix.io/
+Tags: featured image, metabox
+Requires at least: 3.5
+Tested up to: 3.9.1
+Stable tag: 0.1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Custom the title, content and link / button text in the Featured Image metabox.
+
+== Description ==
+
+Custom the title, content and link / button text in the Featured Image metabox.
+
+== Installation ==
+
+= Using The WordPress Dashboard =
+
+1. Navigate to the 'Add New' in the plugins dashboard
+2. Search for 'custom-featured-image-metabox'
+3. Click 'Install Now'
+4. Activate the plugin on the Plugin dashboard
+
+= Uploading in WordPress Dashboard =
+
+1. Navigate to the 'Add New' in the plugins dashboard
+2. Navigate to the 'Upload' area
+3. Select `custom-featured-image-metabox.zip` from your computer
+4. Click 'Install Now'
+5. Activate the plugin in the Plugin dashboard
+
+= Using FTP =
+
+1. Download `custom-featured-image-metabox.zip`
+2. Extract the `custom-featured-image-metabox` directory to your computer
+3. Upload the `custom-featured-image-metabox` directory to the `/wp-content/plugins/` directory
+4. Activate the plugin in the Plugin dashboard
+
+== Screenshots ==
+
+1. A settings page for this plugin
+2. A customized Featured Image metabox
+
+== Changelog ==
+
+= 0.1.0 =
+* The first version
---
+++ custom-featured-image-metabox.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Custom Featured Image Metabox.
+ *
+ * Custom the title, content and link / button text in the Featured Image metabox.
+ *
+ * @package   Custom_Featured_Image_Metabox
+ * @author    1fixdotio <1fixdotio@gmail.com>
+ * @license   GPL-2.0+
+ * @link      http://1fix.io
+ * @copyright 2014 1Fix.io
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Custom Featured Image Metabox
+ * Plugin URI:        http://1fix.io
+ * Description:       Custom the title, content and link / button text in the Featured Image metabox.
+ * Version:           0.1.0
+ * Author:            1fixdotio
+ * Author URI:        http://1fix.io
+ * Text Domain:       cfim
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ * GitHub Plugin URI: https://github.com/1fixdotio/custom-featured-image-metabox
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/*----------------------------------------------------------------------------*
+ * Public-Facing Functionality
+ *----------------------------------------------------------------------------*/
+
+require_once( plugin_dir_path( __FILE__ ) . 'public/class-custom-featured-image-metabox.php' );
+
+/*
+ * Register hooks that are fired when the plugin is activated or deactivated.
+ * When the plugin is deleted, the uninstall.php file is loaded.
+ *
+ */
+register_activation_hook( __FILE__, array( 'Custom_Featured_Image_Metabox', 'activate' ) );
+register_deactivation_hook( __FILE__, array( 'Custom_Featured_Image_Metabox', 'deactivate' ) );
+
+add_action( 'plugins_loaded', array( 'Custom_Featured_Image_Metabox', 'get_instance' ) );
+
+/*----------------------------------------------------------------------------*
+ * Dashboard and Administrative Functionality
+ *----------------------------------------------------------------------------*/
+
+/*
+ * @TODO:
+ *
+ * - replace `class-custom-featured-image-metabox-admin.php` with the name of the plugin's admin file
+ * - replace Custom_Featured_Image_Metabox_Admin with the name of the class defined in
+ *   `class-custom-featured-image-metabox-admin.php`
+ *
+ * If you want to include Ajax within the dashboard, change the following
+ * conditional to:
+ *
+ * if ( is_admin() ) {
+ *   ...
+ * }
+ *
+ * The code below is intended to to give the lightest footprint possible.
+ */
+if ( is_admin() ) {
+
+	require_once( plugin_dir_path( __FILE__ ) . 'admin/class-custom-featured-image-metabox-admin.php' );
+	add_action( 'plugins_loaded', array( 'Custom_Featured_Image_Metabox_Admin', 'get_instance' ) );
+
+}
